da76fdefd3244c9d01b4af8ce8a52094b4e54eb5
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2025-12-03 23:14:57 +0100

    Triangle Pattern

--- Patterns/basicSquare.cpp
+++ Patterns/basicSquare.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int size;
-    cout << "Enter the size of the square: ";
-    cin >> size;
-
+void printSquare(int size) {
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++) {
             cout << "* ";
         }
         cout << endl;
     }
+}
+
+int main() {
+    int size;
+    cout << "Enter the size of the square: ";
+    cin >> size;
+
+    printSquare(size);
 
     return 0;
 }
